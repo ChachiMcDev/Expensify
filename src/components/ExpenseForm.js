6b5b5e1c30b5e78517810780b7d61b6a4b5671de
Chuck Nelson <chachimcdev@gmail.com>
2024-10-01 09:24:25 -0700

    psuedo aotomated i guess

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -5,7 +5,7 @@ import DatePicker from "react-datepicker";
 import { useNavigate } from "react-router-dom";
 import "react-datepicker/dist/react-datepicker.css";
 
-//here we go again
+//not quite as automatic as i wanted but whatever
 
 //const dateNow = dayjs().valueOf();
 
