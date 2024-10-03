6c07da18b5f64846ed1f8db7d45e22f5590bba98
Chuck Nelson <chachimcdev@gmail.com>
2024-10-03 11:15:35 -0700

    changes to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
 const path = require('path')
 
-module.exports = {
-    entry: './src/app.js',
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    devtool: 'source-map',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public')
+
+module.exports = (env) => {
+    const { production } = env;
+
+    return {
+        entry: './src/app.js',
+        mode: production ? 'production' : 'development',
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: 'bundle.js'
         },
-        historyApiFallback: true,
-        compress: true,
-        port: 9000,
-    },
-    module: {
-        rules: [{
-            use: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            use: ['style-loader', 'css-loader', 'sass-loader'],
-            test: /\.s?css$/
-        }]
+        devtool: production ? 'source-map' : 'eval-cheap-source-map',
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'public')
+            },
+            historyApiFallback: true,
+            compress: true,
+            port: 9000,
+        },
+        module: {
+            rules: [{
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+                test: /\.s?css$/
+            }]
+        }
     }
 
 }
